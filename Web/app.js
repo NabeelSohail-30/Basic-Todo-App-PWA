6dd2517ba3769d1343c4108035fab3d32fd41b87
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-01-06 03:22:27 +0500

    almost done

--- Web/app.js
+++ Web/app.js
@@ -12,3 +12,85 @@ if (url.split(":")[0] === 'https') {
 
 
 /*---------------------------------------------------------------*/
+
+
+let getAllTodos = () => {
+    axios.get(`${baseUrl}/todos`)
+        .then(function (response) {
+            // handle success
+            console.log("response is success");
+            console.log(response.data.data);
+            document.querySelector("#todoList").innerHTML = "";
+
+            response?.data?.data.map((eachTodo, index) => {
+                document.querySelector("#todoList").innerHTML +=
+                    `
+                        <div class="row">
+                            <div class="col-11">
+                                <label for="" id="lblTodo">
+                                    ${eachTodo.todo}
+                                </label>
+                            </div>
+                            <div class="col-1">
+                                <button class="btn-del" onclick="deleteTodo(${eachTodo._id});">
+                                    <img src="img/delete.png" alt="">
+                                </button>
+                            </div>
+                        </div>
+                    `
+            })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            alert(error.message);
+        })
+}
+
+
+let addTodo = () => {
+
+    let todo = document.querySelector("#txtTodo").value
+
+    axios.post(`${baseUrl}/todo`, {
+        todo: todo
+    })
+        .then(function (response) {
+            // handle success
+            console.log("response is success");
+            console.log(response.data);
+
+            alert(response.data.message);
+
+            getAllTodos();
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            alert(error.message);
+        })
+
+}
+
+
+let deleteTodo = (id) => {
+
+    axios.delete(`${baseUrl}/todo/${id}`)
+        .then(function (response) {
+            // handle success
+            console.log("response is success");
+            console.log(response.data);
+
+            alert(response.data.message);
+            getAllTodos();
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error.message);
+            alert(error.message);
+        })
+}
+
+getAllTodos();
--- server.mjs
+++ server.mjs
@@ -5,27 +5,104 @@ import mongoose from 'mongoose';
 
 /*------------------------Schema-------------------------------*/
 
-let productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: Number,
-    category: String,
-    description: String,
+
+let todoSchema = new mongoose.Schema({
+    todo: { type: String, required: true },
     createdOn: { type: Date, default: Date.now }
 });
-const productModel = mongoose.model('products', productSchema);
+const todoModel = mongoose.model('dbTodos', todoSchema);
+
 
 /*--------------------------------------------------------------*/
+
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 const port = process.env.PORT || 5001;
 
+
 /*---------------------APIs--------------------------*/
 
 
+app.get('/todos', (req, res) => {
+
+    todoModel.find({})
+        .sort({ _id: -1 })
+        .exec((err, data) => {
+            if (!err) {
+                res.send({
+                    message: "here is you todo list",
+                    data: data
+                })
+            } else {
+                res.status(500).send({
+                    message: "server error"
+                })
+            }
+        });
+})
+
+
+app.post("/todo", (req, res) => {
+
+    const body = req.body;
+    if (
+        !body.todo
+    ) {
+        res.status(400).send(` required parameter missing, todo is required `)
+        return;
+    }
+
+    todoModel.create({
+        todo: body.todo
+    },
+        (err, saved) => {
+            if (!err) {
+                console.log(saved);
+
+                res.send({
+                    message: "your todo is saved"
+                })
+            } else {
+                res.status(500).send({
+                    message: "server error"
+                })
+            }
+        })
+})
+
+
+app.delete('/todo/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    todoModel.deleteOne({ _id: id }, (err, deletedData) => {
+        console.log("deleted: ", deletedData);
+        if (!err) {
+
+            if (deletedData.deletedCount !== 0) {
+                res.send({
+                    message: "todo has been deleted successfully",
+                })
+            } else {
+                res.send({
+                    message: "No todo found with this id: " + id,
+                })
+            }
+
+
+        } else {
+            res.status(500).send({
+                message: "server error"
+            })
+        }
+    });
+})
+
 
 /*--------------------------------------------------------------*/
+
 
 const __dirname = path.resolve();
 app.get('/', express.static(path.join(__dirname, "/Web/index.html")));
@@ -35,12 +112,16 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+
 /*-------------------------Mongodb Connection URL--------------------------------*/
+
 
 let dbURL = 'mongodb+srv://NabeelSohail:Nabeel30@cluster0.lidnkc6.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURL);
 
+
 /*---------------------Mongodb Connected Disconnected Events--------------------------*/
+
 
 mongoose.connection.on('connected', function () {//connected
     console.log("Mongoose is connected");
@@ -63,5 +144,6 @@ process.on('SIGINT', function () {/////this function will run jst before app is 
         process.exit(0);
     });
 });
+
 
 /*--------------------------------------------------------------*/
