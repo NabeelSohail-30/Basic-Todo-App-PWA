3e9a74138647c72b30fee8d7357c1b756d16589f
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-01-07 12:33:33 +0500

    url updated

--- Web/app.js
+++ Web/app.js
@@ -5,7 +5,7 @@ const url = window.location.href;
 let baseUrl = "";
 
 if (url.split(":")[0] === 'https') {
-    baseUrl = 'https://excited-train-deer.cyclic.app/';
+    baseUrl = 'https://pwatodoapp.cyclic.app/';
 } else {
     baseUrl = 'http://localhost:5001';
 }
